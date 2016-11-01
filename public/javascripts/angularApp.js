89f80bab559ea88dbad4640c323cad0ea39c4354
batzner <info@kilians.net>
2016-11-01 10:33:39 +0100

    Initial commit with full stack working basically

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,82 @@
+var app = angular.module('blog', ['ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+        $stateProvider
+            .state('/', {
+                url: '/',
+                templateUrl: '/templates/home.html',
+                controller: 'MainCtrl',
+                resolve: {
+                    postsPromise: ['postsFactory', function(postsFactory) {
+                        return postsFactory.getAll();
+                    }]
+                }
+            })
+            .state('post', {
+                url: '/post/{id}',
+                templateUrl: '/templates/post.html',
+                controller: 'PostCtrl'
+            });
+
+        $urlRouterProvider.otherwise('/');
+        // $locationProvider.html5Mode(true);
+    }
+]);
+
+app.factory('postsFactory', ['$http', function($http) {
+    var o = {
+        posts: []
+    };
+
+    o.getAll = function() {
+        return $http.get('/api/posts').then(
+            function(response) {
+                angular.copy(response.data, o.posts);
+            },
+            function(response) {
+                console.log(response);
+            }
+        );
+    }
+
+    o.create = function(post) {
+        return $http.post('/api/posts', post).success(function(data) {
+            o.posts.push(data);
+        });
+    };
+    return o;
+}])
+
+app.controller('MainCtrl', [
+    '$scope',
+    'postsFactory',
+    function($scope, postsFactory) {
+        $scope.posts = postsFactory.posts;
+        $scope.render = function(e) {
+            return $(e).html();
+        }
+
+        $scope.addPost = function() {
+            postsFactory.create({
+                title: $scope.title,
+                body: $scope.body,
+            });
+            $scope.title = '';
+            $scope.body = '';
+        };
+    }
+]);
+
+app.controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'postsFactory',
+    function($scope, $stateParams, postsFactory) {
+        $scope.post = postsFactory.posts[$stateParams.id];
+    }
+]);
