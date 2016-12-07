89c02d9d56aab6ed95c2c1bfcaca50d2ef98a42d
batznerk <batznerk@amazon.com>
2016-12-07 21:33:04 +0100

    Switched to directly gulping JS and CSS with min suffix and ignoring posts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,7 +10,8 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
-const nodemon = require('gulp-nodemon');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 // Bases for specifying paths in gulp.src and gulp.dest
 const bases = {
@@ -20,25 +21,18 @@ const bases = {
 
 // Paths to be used in gulp.src
 const sourcePaths = {
-    javascripts: ['javascripts/**/*.js'],
-    stylesheets: ['stylesheets/**/*.css'],
-    html: ['templates/**/*.html'],
+    javascripts: ['javascripts/**/*.js', '!javascripts/posts/**/*.js'],
+    stylesheets: ['stylesheets/**/*.css', '!stylesheets/posts/**/*.css'],
+    templates: ['templates/**/*.html'],
     images: ['images/**/*.png', 'images/**/*.jpg', 'images/**/*.jpeg', 'images/**/*.gif'],
     all: ['**/*', '!**/README.md']
 };
 
-// Paths to be used in gulp.dest
-const destPaths = {
-    javascripts: 'javascripts/',
-    stylesheets: 'stylesheets/',
-    html: 'templates/',
-    images: 'images/'
-};
+// Files that shall not directly be copied
+const excludeLists = [sourcePaths.javascripts, sourcePaths.stylesheets, sourcePaths.images,
+    sourcePaths.templates];
 
 const minJsName = 'app.min.js';
-
-// ESLint ignores:
-const eslintIgnores = ['!**/visualizing-travel-times-with-multidimensional-scaling/**/*'];
 
 // Delete the dist directory
 gulp.task('_clean', function () {
@@ -46,34 +40,78 @@ gulp.task('_clean', function () {
         .pipe(clean());
 });
 
-// Copy the public directory to dist. We are copying all files to make sure, we don't miss files
-// that get added in the future.
-gulp.task('_copy', ['_clean'], function () {
-    return gulp.src(sourcePaths.all, {cwd: bases.frontend})
-        .pipe(gulp.dest(bases.dist));
-});
-
 // Transpile the javascript files to ES5. Then, minify them.
-gulp.task('_javascripts', ['_copy'], function () {
-    return gulp.src(sourcePaths.javascripts, {cwd: bases.dist})
+gulp.task('_javascripts', ['_clean'], function () {
+    return gulp.src(sourcePaths.javascripts, {base: bases.frontend, cwd:bases.frontend})
         .pipe(babel())
         .pipe(uglify())
         .pipe(concat(minJsName))
-        .pipe(gulp.dest(bases.dist+destPaths.javascripts));
+        .pipe(gulp.dest(bases.dist+'javascripts/'));
+});
+
+// Minify the stylesheets
+gulp.task('_stylesheets', ['_clean'], function () {
+    return gulp.src(sourcePaths.stylesheets, {base: bases.frontend, cwd:bases.frontend})
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(bases.dist));
 });
 
 // Minify the images
-gulp.task('_images', ['_copy'], function () {
-    return gulp.src(sourcePaths.images, {cwd: bases.dist})
+gulp.task('_images', ['_clean'], function () {
+    return gulp.src(sourcePaths.images, {base: bases.frontend, cwd:bases.frontend})
         .pipe(imagemin())
-        .pipe(gulp.dest(bases.dist+destPaths.images));
+        .pipe(gulp.dest(bases.dist));
+});
+
+// Copy the templates
+gulp.task('_templates', ['_clean'], function () {
+    return gulp.src(sourcePaths.templates, {base: bases.frontend, cwd:bases.frontend})
+        .pipe(gulp.dest(bases.dist));
+});
+
+// Copy the rest
+gulp.task('_rest:all', ['_clean'], function () {
+    // Copy all files except JS, CSS, images and templates
+    let files = Array.from(sourcePaths.all);
+    // Exlude all files from the exclude lists by adding them to files with an '!'
+    excludeLists.forEach(list => {
+        list.forEach(path => files.push('!'+path));
+    });
+
+    return gulp.src(files, {base: bases.frontend, cwd:bases.frontend})
+        .pipe(gulp.dest(bases.dist));
+});
+
+// Copy the excluded files from the exclude lists (for example javascripts/posts/*)
+gulp.task('_rest:excluded', ['_clean'], function () {
+    let files = [];
+    excludeLists.forEach(list => {
+        list.forEach(path => {
+            if (path.startsWith('!')) files.push(path.substr(1));
+        });
+    });
+
+    return gulp.src(files, {base: bases.frontend, cwd:bases.frontend})
+        .pipe(gulp.dest(bases.dist));
+});
+
+gulp.task('_rest', ['_rest:all', '_rest:excluded']);
+
+// Default task producing a ready-to-ship frontend in the dist folder
+gulp.task('default', ['_javascripts', '_stylesheets', '_images', '_templates', '_rest']);
+
+// Copy everything (only for dev)
+gulp.task('_copy', ['_clean'], function () {
+    return gulp.src(sourcePaths.all, {base: bases.frontend, cwd:bases.frontend})
+        .pipe(gulp.dest(bases.dist));
 });
 
 // Transpile the javascript files to ES5.
 gulp.task('babel', ['_copy'], function () {
-    return gulp.src(sourcePaths.javascripts, {cwd: bases.dist})
+    return gulp.src(sourcePaths.javascripts, {base: bases.frontend, cwd:bases.frontend})
         .pipe(babel())
-        .pipe(gulp.dest(bases.dist+destPaths.javascripts));
+        .pipe(gulp.dest(bases.dist));
 });
 
 // Watch the public directory in order to move changes directly to dist. Also, apply babel.
@@ -83,7 +121,7 @@ gulp.task('babel-watch', ['babel'], function () {
 
 // Check code style on JS
 gulp.task('eslint', function () {
-    gulp.src(sourcePaths.javascripts.concat(eslintIgnores), {cwd: bases.frontend})
+    gulp.src(sourcePaths.javascripts, {cwd: bases.frontend})
         .pipe(eslint())
         .pipe(eslint.format());
 });
@@ -92,6 +130,3 @@ gulp.task('eslint', function () {
 gulp.task('dev', ['_copy'], function () {
     gulp.watch(['public/**/*'], ['_copy']);
 });
-
-// Default task producing a ready-to-ship frontend in the dist folder
-gulp.task('default', ['_javascripts', '_images']);
