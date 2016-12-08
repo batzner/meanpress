4a41f022f458dc5b557b4736e492e57ac817953b
batznerk <batznerk@amazon.com>
2016-12-08 20:56:18 +0100

    Fixed error messages in login and register

--- public/javascripts/controllers/auth.js
+++ public/javascripts/controllers/auth.js
@@ -22,13 +22,13 @@ class AuthCtrl extends InjectionReceiver {
     register() {
         // Call the backend to register. Go home if succeeded.
         this.AuthService.register(this.$scope.user)
-            .then(() => this.$state.go('home'), this.onError);
+            .then(() => this.$state.go('home'), response => this.onError(response));
     }
 
     login() {
         // Call the backend to login. Go home if succeeded.
         this.AuthService.login(this.$scope.user)
-            .then(() => this.$state.go('home'), this.onError);
+            .then(() => this.$state.go('home'), response => this.onError(response));
     }
 }
 
